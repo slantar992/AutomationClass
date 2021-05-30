04678abf6e76b42a6960023057a6ead9e8829fb2
Slantar <albertouma92@gmail.com>
2021-05-30 16:37:42 +0200

    Add build method

---
+++ game/Assets/Scripts/Editor/BuildAutomation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public static class BuildAutomation
+{
+	[MenuItem("Test/Debug")]
+	public static void Build()
+	{
+		Debug.Log("hola");
+	}
+}
