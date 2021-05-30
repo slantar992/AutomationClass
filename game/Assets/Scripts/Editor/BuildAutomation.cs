c5a63c9ad93390d9c94a99d6ac189b321fd05657
Slantar <albertouma92@gmail.com>
2021-05-30 17:30:27 +0200

    Add project environment variable

--- game/Assets/Scripts/Editor/BuildAutomation.cs
+++ game/Assets/Scripts/Editor/BuildAutomation.cs
@@ -4,13 +4,17 @@ using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 
 public static class BuildAutomation
 {
+
 	[MenuItem("Tools/BuildToSteam")]
 	public static void Build()
 	{
-		
-		BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, Application.dataPath + "/../Builds/Steam/game.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+		var AutomationProjectPath = Environment.GetEnvironmentVariable("AUTOMATION_PROJECT_PATH", EnvironmentVariableTarget.User);
+		var buildPath = $"{AutomationProjectPath}/game/Builds/Steam";
+		BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, $"{buildPath}/game.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+		Application.OpenURL(buildPath);
 	}
 }
