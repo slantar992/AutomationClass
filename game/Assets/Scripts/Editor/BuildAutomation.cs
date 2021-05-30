40dce24fbb0d488b206e4482eaea09508e657b99
Slantar <albertouma92@gmail.com>
2021-05-30 18:18:49 +0200

    Add steam upload and log process

--- game/Assets/Scripts/Editor/BuildAutomation.cs
+++ game/Assets/Scripts/Editor/BuildAutomation.cs
@@ -5,6 +5,8 @@ using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System;
+using System.Diagnostics;
+using System.IO;
 
 public static class BuildAutomation
 {
@@ -13,8 +15,25 @@ public static class BuildAutomation
 	public static void Build()
 	{
 		var AutomationProjectPath = Environment.GetEnvironmentVariable("AUTOMATION_PROJECT_PATH", EnvironmentVariableTarget.User);
-		var buildPath = $"{AutomationProjectPath}/game/Builds/Steam";
+		var buildPath = $"{AutomationProjectPath}/tools/Build";
+		var logPath = $"{AutomationProjectPath}/log.txt";
+		File.Create(logPath);
+
+		//Build game
+		File.AppendAllText(logPath, "Building...");
 		BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, $"{buildPath}/game.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
-		Application.OpenURL(buildPath);
+		File.AppendAllText(logPath, "Build Successfull");
+
+		//Upload to steam
+		var startInfo = new ProcessStartInfo
+		{
+			FileName = $"{AutomationProjectPath}/tools/UploadToSteam.bat",
+			Arguments = ""
+		};
+
+		var process = new Process { StartInfo = startInfo };
+
+		process.Start();
+		process.WaitForExit();
 	}
 }
